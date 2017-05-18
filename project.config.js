7e831f1b95b644d22d86feb582c811c787790e65
Brykou <brice.leydet@webedia-group.com>
2017-05-18 23:10:33 +0200

    Hotfix V1
    Fix a broken link on ressources page.
    Fix a typo on homepage.

--- project.config.js
+++ project.config.js
@@ -1,7 +1,7 @@
 export default {
   siteName: "La Baleine",
   headline: "Communauté francophone de crypto monnaies",
-  opening: 'Bienvenu sur La Baleine, une communauté tournée autour des crypto monnaies, et du trading. Débutant ou confirmé, Rejoignez-nous pour améliorer vos compétences, et vos gains.',
+  opening: 'Bienvenue sur La Baleine, une communauté tournée autour des crypto monnaies, et du trading. Débutant ou confirmé, Rejoignez-nous pour améliorer vos compétences, et vos gains.',
   twitterUrl: 'https://twitter.com/BaleineFR',
   twitchUrl: 'https://www.twitch.tv/baleinefr',
   youtubeUrl: 'https://www.youtube.com/channel/UCdEgqcbima-Bv5jnArrXiKA?feature=hovercard?&ab_channel=LaBaleine',
