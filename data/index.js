91113de64daecc79234eb7f0d5082b9b79a68e4d
Brykou <brice.leydet@webedia-group.com>
2017-05-19 23:43:18 +0200

    Refactor of resource page
    Move data of ressource page to a json file.
    It allows to have a cleaner Ressource component,
    and easily add new roussources.

--- data/index.js
+++ data/index.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
+import ressources from './ressources';
 
-var axiosInstance = axios.create({
+var baleineBotApi = axios.create({
   baseURL: 'https://api.baleine.036.fr'
 });
 
-
-async function getGlossary(limit){
-  const glossary = await axiosInstance.get('/word-definitions');
+async function getGlossary(){
+  const glossary = await baleineBotApi.get('/word-definitions');
   return glossary.data;
+}
+
+function getRessources(){
+  return ressources;
 }
 
 export {
   getGlossary,
+  getRessources,
 };
