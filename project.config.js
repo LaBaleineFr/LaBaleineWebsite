f55ddf670ad3e081dc0f3dfc406f2c5c5ad0c0a1
Brykou <11095630+Brykou@users.noreply.github.com>
2020-05-11 21:41:58 +0900

    Remove unused and outdated components and update Discord

--- project.config.js
+++ project.config.js
@@ -4,10 +4,11 @@ export default {
   opening: `Bienvenue sur La Baleine, une communauté tournée vers les crypto monnaies, et le trading.
   L'entraide et le partage font partie des valeurs qui sont au coeur de La Baleine.
   C'est pourquoi notre équipe vous propose un ensemble d'outils, et de supports de formation pour vous améliorer.`,
-  twitterUrl: 'https://twitter.com/BaleineFR',
-  twitchUrl: 'https://www.twitch.tv/baleinefr',
-  youtubeUrl: 'https://www.youtube.com/channel/UCdEgqcbima-Bv5jnArrXiKA?feature=hovercard?&ab_channel=LaBaleine',
-  discordUrl: 'https://discord.gg/mRrX3tV',
-  githubUrl: 'https://github.com/LaBaleineFr',
-  email: 'baleinefr@gmail.com',
-}
+  twitterUrl: "https://twitter.com/BaleineFR",
+  twitchUrl: "https://www.twitch.tv/baleinefr",
+  youtubeUrl:
+    "https://www.youtube.com/channel/UCdEgqcbima-Bv5jnArrXiKA?feature=hovercard?&ab_channel=LaBaleine",
+  discordUrl: "https://discord.gg/tUNqsq8",
+  githubUrl: "https://github.com/LaBaleineFr",
+  email: "baleinefr@gmail.com",
+};
